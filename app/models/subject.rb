6bca9db23649fcf5b581b86b890c7b9409f4a383
Campbell Allen <campbell.allen@gmail.com>
2022-02-10 16:38:06 +0000

    ensure FK attributes are present

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
 class Subject < ApplicationRecord
+  validates :subject_id, :workflow_id, :project_id, presence: true
 end
--- spec/models/subject_spec.rb
+++ spec/models/subject_spec.rb
@@ -1,7 +1,29 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Subject, type: :model do
+  let(:attributes) do
+    { subject_id: 1, workflow_id: 2, project_id: 3 }
+  end
+  let(:model) { described_class.new(attributes) }
+
   it 'creates a valid model' do
-    expect(Subject.new).to be_valid
+    expect(model).to be_valid
+  end
+
+  it 'is invalid without a subject_id' do
+    model.subject_id = nil
+    expect(model).to be_invalid
+  end
+
+  it 'is invalid without a workflow_id' do
+    model.workflow_id = nil
+    expect(model).to be_invalid
+  end
+
+  it 'is invalid without a project_id' do
+    model.project_id = nil
+    expect(model).to be_invalid
   end
 end
