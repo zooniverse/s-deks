31e51d98c0202b1cd0dc49d1c4703906c2c5a1ce
Campbell Allen <campbell.allen@gmail.com>
2022-02-17 13:38:50 +0000

    do not delete contexts with live subejcts

--- app/models/context.rb
+++ app/models/context.rb
@@ -1,5 +1,5 @@
 class Context < ApplicationRecord
-  has_many :subjects
+  has_many :subjects, dependent: :restrict_with_exception
 
   validates :workflow_id, uniqueness: { scope: :project_id, message: 'Workflow and project must be unique' }
 end
--- spec/models/context_spec.rb
+++ spec/models/context_spec.rb
@@ -12,18 +12,33 @@ RSpec.describe Context, type: :model do
     expect(context).to be_valid
   end
 
-  it 'does not allow duplicates' do
+  it 'does not allow duplicate workflow project ids' do
     context.save!
     dup = described_class.new(attributes)
-    expect(dup).to be_invalid
+    dup.valid?
     expect(dup.errors[:workflow_id]).to include('Workflow and project must be unique')
   end
 
+  it 'does not raise an error deleting without subjects' do
+    expect { context.destroy }.not_to raise_error
+  end
+
   describe '.subjects' do
-    it 'correctly links the association' do
+    let(:subject_model) do
+      Subject.create({ subject_id: 1, context_id: context.id })
+    end
+
+    before do
       context.save
-      subject_model = Subject.create({ subject_id: 1, context_id: context.id })
+      subject_model
+    end
+
+    it 'correctly links the association' do
       expect(context.subjects).to match_array([subject_model])
+    end
+
+    it 'raises an error when destroying' do
+      expect { context.destroy }.to raise_error(ActiveRecord::DeleteRestrictionError, 'Cannot delete record because of dependent subjects')
     end
   end
 end
