bc30d719f2d2a0eae499a7db3c4fe3baa035d408
Campbell Allen <campbell.allen@gmail.com>
2022-03-15 15:46:35 +0000

    align to zoobot container prefixes

--- spec/services/export/training_data_csv_spec.rb
+++ spec/services/export/training_data_csv_spec.rb
@@ -39,7 +39,7 @@ RSpec.describe Export::TrainingDataCsv do
     end
 
     it 'returns the csv data in the temp file' do
-      expected_output = "id_str,file_loc,smooth-or-featured_smooth,smooth-or-featured_featured-or-disk,smooth-or-featured_artifact\n8000_231121_468,/training_images/2f2490b4-65c1-4dca-ba25-c44128aa7a39.jpeg,3,9,0\n"
+      expected_output = "id_str,file_loc,smooth-or-featured_smooth,smooth-or-featured_featured-or-disk,smooth-or-featured_artifact\n8000_231121_468,/test/training_images/2f2490b4-65c1-4dca-ba25-c44128aa7a39.jpeg,3,9,0\n"
       results = File.read(exporter.dump.path)
       expect(results).to match(expected_output)
     end
@@ -53,7 +53,7 @@ RSpec.describe Export::TrainingDataCsv do
       end
 
       it 'returns the multi image csv data in the temp file' do
-        expected_output = "id_str,file_loc,smooth-or-featured_smooth,smooth-or-featured_featured-or-disk,smooth-or-featured_artifact\n8000_231121_468,/training_images/2f2490b4-65c1-4dca-ba25-c44128aa7a39.jpeg,3,9,0\n8000_231121_468,/training_images/fdccb1cf-0fc9-49b5-b054-62c83bccb9cd.jpeg,3,9,0"
+        expected_output = "id_str,file_loc,smooth-or-featured_smooth,smooth-or-featured_featured-or-disk,smooth-or-featured_artifact\n8000_231121_468,/test/training_images/2f2490b4-65c1-4dca-ba25-c44128aa7a39.jpeg,3,9,0\n8000_231121_468,/test/training_images/fdccb1cf-0fc9-49b5-b054-62c83bccb9cd.jpeg,3,9,0"
         results = File.read(exporter.dump.path)
         expect(results).to match(expected_output)
       end
