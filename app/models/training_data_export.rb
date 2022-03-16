21970b8cce605fbec8e299f7ff505a4aa6d1a6e7
Campbell Allen <campbell.allen@gmail.com>
2022-03-16 17:02:40 +0000

    change default to started

--- app/models/training_data_export.rb
+++ app/models/training_data_export.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class TrainingDataExport < ApplicationRecord
-  # default is pending via the migration default: 0
-  enum :state, %i[pending started finished failed]
+  # default is started via the migration default: 0
+  enum :state, %i[started finished failed]
 
   validates :state, presence: true
 
