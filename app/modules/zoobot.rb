93ea32923caa49ddb94689f266ea868bd43899e1
Campbell Allen <campbell.allen@gmail.com>
2022-03-21 14:08:47 +0000

    add training image path to zoobot module

--- app/modules/zoobot.rb
+++ app/modules/zoobot.rb
@@ -1,12 +1,16 @@
 # frozen_string_literal: true
 
 module Zoobot
-  CONTAINER_TRAINING_PATH_PREFIX = "/#{Rails.env}/training_images"
+  CONTAINER_TRAINING_PATH_PREFIX = 'training_images'
   CONTAINER_PATH_PREFIX = "/#{Rails.env}/training_catalogues"
 
+  def self.training_image_path(image_url)
+    "#{CONTAINER_TRAINING_PATH_PREFIX}/#{File.basename(image_url)}"
+  end
+
   def self.container_image_path(image_url)
-    image_file_name = File.basename(image_url)
-    "#{CONTAINER_TRAINING_PATH_PREFIX}/#{image_file_name}"
+    image_file_name = training_image_path(image_url)
+    "/#{Rails.env}/#{image_file_name}"
   end
 
   def self.storage_path_key(workflow_id)
--- spec/modules/zoobot_spec.rb
+++ spec/modules/zoobot_spec.rb
@@ -7,6 +7,15 @@ RSpec.describe Zoobot do
     'https://panoptes-uploads.zooniverse.org/subject_location/2f2490b4-65c1-4dca-ba25-c44128aa7a39.jpeg'
   end
 
+  describe '.training_image_path' do
+    let(:expected_path) { 'training_images/2f2490b4-65c1-4dca-ba25-c44128aa7a39.jpeg' }
+
+    it 'converts the url to a training container path' do
+      extracted_path = described_class.training_image_path(image_url)
+      expect(extracted_path).to eq(expected_path)
+    end
+  end
+
   describe '.container_image_path' do
     let(:expected_path) { '/test/training_images/2f2490b4-65c1-4dca-ba25-c44128aa7a39.jpeg' }
 
