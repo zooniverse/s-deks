3aa14962f609878852d3be9de3b83f95e8ac3100
Campbell Allen <campbell.allen@gmail.com>
2022-03-24 16:01:58 +0000

    allow list non attribute json payload params

--- app/controllers/user_reductions_controller.rb
+++ app/controllers/user_reductions_controller.rb
@@ -4,6 +4,10 @@ class UserReductionsController < ApplicationController
   # as we're running in API mode we need to include basic auth
   include ActionController::HttpAuthentication::Basic::ControllerMethods
 
+  # ensure we include the non model attributes in the user_reduction nested payload
+  # https://api.rubyonrails.org/v7.0.2.3/classes/ActionController/ParamsWrapper.html
+  wrap_parameters UserReduction, include: %w[id reducible data subject]
+
   http_basic_authenticate_with(
     name: Rails.application.config.user_reduction_basic_auth_username,
     password: Rails.application.config.user_reduction_basic_auth_password,
@@ -11,6 +15,11 @@ class UserReductionsController < ApplicationController
   )
 
   def create
+    debugger
+    # so the payload doesn't conform to the expected format for strong params
+    # Started POST "/user_reductions" for 10.244.6.79 at 2022-03-24 15:36:58 +0000
+    # I, [2022-03-24T15:36:58.576861 #1]  INFO -- : [3c9799100bace703fe82f97187bc97a8] Processing by UserReductionsController#create as JSON
+    # I, [2022-03-24T15:36:58.577022 #1]  INFO -- : [3c9799100bace703fe82f97187bc97a8]   Parameters: {"id"=>1659, "reducible"=>{"id"=>3598, "type"=>"Workflow"}, "data"=>{"1"=>1}, "subject"=>{"id"=>85082, "metadata"=>{"!Image"=>"http://skyserver.sdss.org/dr8/en/tools/explore/obj.asp?id=1237648721216078463", "!SDSS_ID"=>"1237648721216078463", "Galaxy_ID"=>"5", "image_file"=>"1237648721216078463.jpeg", "!GZ_Original_Merger"=>"0", "!GZ_Original_Spiral"=>"31", "!GZ_Original_Artifact"=>"1", "!GZ_Original_Elliptical"=>"0", "!Computer_Classification"=>"Not Elliptical"}, "created_at"=>"2020-03-30T19:55:06.539Z", "updated_at"=>"2020-03-30T19:55:06.539Z"}, "reducer_key"=>"[FILTERED]", "created_at"=>"2022-03-24T15:24:10.052Z", "updated_at"=>"2022-03-24T15:24:10.052Z", "user_reduction"=>{"id"=>1659, "created_at"=>"2022-03-24T15:24:10.052Z", "updated_at"=>"2022-03-24T15:24:10.052Z"}}
     user_reduction = Import::UserReduction.new(user_reduction_params).run
     render status: :created, json: user_reduction.to_json
   end
--- spec/requests/user_reductions_spec.rb
+++ spec/requests/user_reductions_spec.rb
@@ -54,6 +54,36 @@ RSpec.describe 'UserReductions', type: :request do
       expect(json_parsed_response_body.keys).to match_array(expected_attributes)
     end
 
+    context 'with unrwapped params' do
+      let(:user_reduction_json_payload) do
+      {
+        id: 4,
+        reducible: {
+          id: context.workflow_id,
+          type: 'Workflow'
+        },
+        data: {
+          '0' => 3,
+          '1' => 9,
+          '2' => 0
+        },
+        subject: {
+          id: 999,
+          metadata: { '#name' => '8000_231121_468' },
+          created_at: '2021-08-06T11:08:53.918Z',
+          updated_at: '2021-08-06T11:08:53.918Z'
+        },
+        created_at: '2021-08-06T11:08:54.000Z',
+        updated_at: '2021-08-06T11:08:54.000Z'
+      }.to_json
+    end
+
+      it 'returns the created response' do
+      create_request
+      expect(response).to have_http_status(:created)
+    end
+    end
+
     context 'with invalid authentication credentials' do
       let(:request_headers) do
         json_headers_with_basic_auth('unknown', 'credentials')
