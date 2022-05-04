614335da53cfb530af12a325f0bf84c13b463725
Campbell Allen <campbell.allen@gmail.com>
2022-05-04 17:35:18 +0100

    allow gz task specifc label lookups
    adds the whole task decision tree schema for data labels and task prefixes

--- app/modules/label_extractors/galaxy_zoo.rb
+++ app/modules/label_extractors/galaxy_zoo.rb
@@ -2,40 +2,68 @@
 
 module LabelExtractors
   class GalaxyZoo
-    class UnkonwnLabelKey < StandardError; end
+    class UnknownTaskKey < StandardError; end
+    class UnknownLabelKey < StandardError; end
 
-    GZ_LABEL_KEY_PREFIX = 'smooth-or-featured_'
-    GZ_TASK_KEY_MAPPING = {
-      '0' => 'smooth',
-      '1' => 'featured-or-disk',
-      '2' => 'artifact'
+    attr_reader :task_schema_lookup_key, :task_prefix_label
+
+    # TODO: add all GZ decision tree task label lookups here
+    # staging: T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10
+    # production: T0, T1, T2, T3, T4, T5, T6, T7, T8, T10, T11
+    #
+    # note: as the workflow question task key's don't change often they
+    # have been hardcoded for now, can switch to dynamic lookup if needed
+    TASK_LABEL_KEY_PREFIXES = {
+      'T0' => 'smooth-or-featured'
     }.freeze
+    TASK_KEY_DATA_LABELS = {
+      'T0' => {
+        '0' => 'smooth',
+        '1' => 'featured-or-disk',
+        '2' => 'artifact'
+      }
+    }.freeze
+
+    # convert this and extract method to an instance vs static class method
+    # and use the injected task_schema_lookup_key
+    # to determine which
+    def initialize(task_schema_lookup_key)
+      @task_schema_lookup_key = task_schema_lookup_key
+      @task_prefix_label = task_prefix
+    end
 
     # extract the keys from the user_reduction data hash
     # and convert the keys to the workflow question tasks
     #
-    # note: as the workflow question task key's don't change often they
-    # have been hardcoded for now, can switch to dynamic lookup if needed
-    def self.extract(data_hash)
-      # e.g. workflow type (GZ) is question type tasks (T0 task specifically)
-      # which correlates to 3 exclusive answers:
-      # 0 (smooth)
-      # 1 (features or disk)
-      # 2 (star or artifact)
-      #
-      # asked mike about the enhanced workflow
-      # it only uses T0 data but we collect all task data in the raw classifications (T0 - TN)
-      # however caesar only extracts / reduces T0 data
-      # and Mike confirmed my understanding
-      # that only task T0 is what Zoobot / Active Learning Loop is training and classifying on
-      #
+    # e.g. workflow type (GZ) are question type 'decision tree' tasks
+    # looking at the 'T0' task it correlates to 3 exclusive answers:
+    # 0 (smooth)
+    # 1 (features or disk)
+    # 2 (star or artifact)
+    #
+    # then combined with the label prefix used to identify the correlated task name for Zoobot
+    def extract(data_hash)
       # use the known catalogue schema for Zoobot decals
       # https://github.com/mwalmsley/zoobot/blob/1a4f48105254b3073b6e3cb47014c6db938ba73f/zoobot/label_metadata.py#L32
       data_hash.transform_keys do |key|
-        raise UnkonwnLabelKey, "key not found: #{key}" unless GZ_TASK_KEY_MAPPING[key]
-
-        "#{GZ_LABEL_KEY_PREFIX}#{GZ_TASK_KEY_MAPPING[key]}"
+        "#{task_prefix_label}_#{data_payload_label(key)}"
       end
+    end
+
+    private
+
+    def task_prefix
+      prefix = TASK_LABEL_KEY_PREFIXES[task_schema_lookup_key]
+      raise UnknownTaskKey, "key not found: #{task_schema_lookup_key}" unless prefix
+
+      prefix
+    end
+
+    def data_payload_label(key)
+      label = TASK_KEY_DATA_LABELS.dig(task_schema_lookup_key, key)
+      raise UnknownLabelKey, "key not found: #{key}" unless label
+
+      label
     end
   end
 end
--- spec/modules/label_extractors/galaxy_zoo_spec.rb
+++ spec/modules/label_extractors/galaxy_zoo_spec.rb
@@ -3,6 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe LabelExtractors::GalaxyZoo do
+  let(:task_schema_lookup_key) { 'T0' }
   let(:data_payload) do
     {
       '0' => 3, # smooth
@@ -11,8 +12,9 @@ RSpec.describe LabelExtractors::GalaxyZoo do
     }
   end
 
-  describe '.extract' do
-    let(:extracted_labels) { described_class.extract(data_payload) }
+  describe '#extract' do
+    let(:extractor_instance) { described_class.new(task_schema_lookup_key) }
+    let(:extracted_labels) { extractor_instance.extract(data_payload) }
     let(:expected_labels) do
       {
         'smooth-or-featured_smooth' => 3,
@@ -22,15 +24,21 @@ RSpec.describe LabelExtractors::GalaxyZoo do
     end
 
     it 'converts the keys to labels list' do
-      extracted_labels = described_class.extract(data_payload)
+      extracted_labels = extractor_instance.extract(data_payload)
       expect(extracted_labels).to match(expected_labels)
     end
 
-    it 'raises and error if the key is not known' do
+    it 'raises an error if the payload data key is not known' do
       unknown_key_payload = data_payload.merge('3' => 0)
       expect {
-        described_class.extract(unknown_key_payload)
-      }.to raise_error(LabelExtractors::GalaxyZoo::UnkonwnLabelKey, 'key not found: 3')
+        extractor_instance.extract(unknown_key_payload)
+      }.to raise_error(LabelExtractors::GalaxyZoo::UnknownLabelKey, 'key not found: 3')
+    end
+
+    it 'raises an error if the task key is not found in the known schema' do
+      expect {
+        described_class.new('T12').extract(data_payload)
+      }.to raise_error(LabelExtractors::GalaxyZoo::UnknownTaskKey, 'key not found: T12')
     end
   end
 end
