481c77da3d013a1861f772161a8201b06b214f3a
Campbell Allen <campbell.allen@gmail.com>
2022-05-04 18:54:56 +0100

    add controller error response handling
    return formatted errors to the client in a useful format

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,20 @@
 class ApplicationController < ActionController::API
+  rescue_from LabelExtractors::GalaxyZoo::UnknownTaskKey, LabelExtractors::Finder::UknownExtractor do |e|
+    json_error_render(:unprocessable_entity, e)
+  end
+
   private
 
   # allow the page size to be set via ?page_size=10 query param
   # but clamp it between 0 and 100 to ensure a reasonable result set
   def params_page_size
     (params[:page_size] || 10).to_i.clamp(0, 100)
+  end
+
+  def json_error_render(status, exception)
+    render(
+      status: status,
+      json: { 'errors' => [{ 'type' => exception.class, 'detail' => exception.message }] }
+    )
   end
 end
