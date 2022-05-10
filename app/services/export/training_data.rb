754ccfc2e70290d5e102852b50e24cc7b879aa9a
Campbell Allen <campbell.allen@gmail.com>
2022-05-10 18:41:52 +0100

    inject gz column headers to exporter

--- app/services/export/training_data.rb
+++ app/services/export/training_data.rb
@@ -10,7 +10,10 @@ module Export
 
     def run
       # create the formatted csv file export IO object
-      csv_export_file = Format::TrainingDataCsv.new(training_data_export.workflow_id).run
+      csv_export_file = Format::TrainingDataCsv.new(
+        training_data_export.workflow_id,
+        Zoobot.gz_label_column_headers
+      ).run
 
       # upload the export file via active storage
       # ensure we specify a known key here so we know the location on contianer storage
