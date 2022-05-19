925aff5004e2fe332089ccb4a26cdb6010fc9f3e
Campbell Allen <campbell.allen@gmail.com>
2022-05-19 16:53:31 +0100

    remove task_key from payload

--- spec/requests/reductions_spec.rb
+++ spec/requests/reductions_spec.rb
@@ -128,7 +128,6 @@ RSpec.describe 'Reductions', type: :request do
           created_at: '2021-08-06T11:08:53.918Z',
           updated_at: '2021-08-06T11:08:53.918Z'
         },
-        task_key: 'T0',
         reducer_key: 'caesar-reducer-id',
         created_at: '2021-08-06T11:08:54.000Z',
         updated_at: '2021-08-06T11:08:54.000Z'
