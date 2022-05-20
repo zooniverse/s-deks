0fcbc964e1708805b5fbc1f40715e68603affeb5
Campbell Allen <campbell.allen@gmail.com>
2022-05-20 12:09:30 +0100

    switch db finder to stub to avoid flaky specs
    CI system having issues with the DB data not being available for the actual spec tests

--- spec/sidekiq/training_data_syncer_job_spec.rb
+++ spec/sidekiq/training_data_syncer_job_spec.rb
@@ -7,7 +7,8 @@ RSpec.describe TrainingDataSyncerJob, type: :job do
     let(:src_image_url_1) { 'https://example.com/image.jpeg' }
     let(:src_image_url_2) { 'https://example.com/image.png' }
     let(:subject_to_sync) do
-      Subject.create(
+      Subject.new(
+        id: 1,
         zooniverse_subject_id: 99,
         context_id: 1,
         locations: [{ 'image/jpeg': src_image_url_1 }, { 'image/png': src_image_url_2 }]
@@ -20,6 +21,7 @@ RSpec.describe TrainingDataSyncerJob, type: :job do
 
     before do
       subject_to_sync
+      allow(Subject).to receive(:find).with(subject_to_sync.id).and_return(subject_to_sync)
       allow(training_data_sync_service_1).to receive(:run)
       allow(training_data_sync_service_2).to receive(:run)
       allow(Storage::TrainingDataSync).to receive(:new).with(src_image_url_1).and_return(training_data_sync_service_1)
