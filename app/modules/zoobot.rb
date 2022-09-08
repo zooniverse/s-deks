c3a447f78b822c2e752069fdf01d8809aea4ddc7
Campbell Allen <campbell.allen@gmail.com>
2022-09-08 18:15:26 +0300

    use new blob storage container / paths structure

--- app/modules/zoobot.rb
+++ app/modules/zoobot.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 module Zoobot
-  CONTAINER_TRAINING_PATH_PREFIX = 'training_images'
-  CONTAINER_PATH_PREFIX = "/#{Rails.env}/training_catalogues"
+  AZ_CONTAINER_NAME = ENV.fetch('AZ_CONTAINER_NAME', 'training')
+  CONTAINER_CATALOG_PATH_PREFIX = "#{AZ_CONTAINER_NAME}/catalogues/#{Rails.env}"
+  CONTAINER_IMAGE_PATH_PREFIX = "#{CONTAINER_CATALOG_PATH_PREFIX}/images"
 
   def self.training_image_path(image_url)
-    "#{CONTAINER_TRAINING_PATH_PREFIX}/#{File.basename(image_url)}"
+    "#{CONTAINER_IMAGE_PATH_PREFIX}/#{File.basename(image_url)}"
   end
 
   def self.container_image_path(image_url)
@@ -14,7 +15,7 @@ module Zoobot
   end
 
   def self.storage_path_key(workflow_id)
-    "#{CONTAINER_PATH_PREFIX}/workflow-#{workflow_id}"
+    "#{CONTAINER_CATALOG_PATH_PREFIX}/workflow-#{workflow_id}"
   end
 
   def self.gz_label_column_headers
