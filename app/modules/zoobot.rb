8db6ca6a045b2c256fbfbee14d4f96f956c1e1e5
Campbell Allen <campbell.allen@gmail.com>
2022-09-09 13:53:07 +0300

    re org zoobot modules to encapsulate storage responsiblity

--- app/modules/zoobot.rb
+++ app/modules/zoobot.rb
@@ -1,24 +1,32 @@
 # frozen_string_literal: true
 
 module Zoobot
-  AZ_CONTAINER_NAME = ENV.fetch('AZ_CONTAINER_NAME', 'training')
-  CONTAINER_CATALOG_PATH_PREFIX = "#{AZ_CONTAINER_NAME}/catalogues/#{Rails.env}"
-  CONTAINER_IMAGE_PATH_PREFIX = "#{CONTAINER_CATALOG_PATH_PREFIX}/images"
-
-  def self.training_image_path(image_url)
-    "#{CONTAINER_IMAGE_PATH_PREFIX}/#{File.basename(image_url)}"
-  end
-
-  def self.container_image_path(image_url)
-    prefix = ENV.fetch('TRAINING_PATH_PREFIX', "/#{Rails.env}")
-    "#{prefix}/#{File.basename(image_url)}"
-  end
-
-  def self.storage_path_key(workflow_id)
-    "/#{CONTAINER_CATALOG_PATH_PREFIX}/workflow-#{workflow_id}"
-  end
-
   def self.gz_label_column_headers
     %w[id_str file_loc] | LabelExtractors::GalaxyZoo.question_answers_schema
+  end
+
+  module Storage
+    AZ_CONTAINER_NAME = ENV.fetch('AZ_CONTAINER_NAME', 'training')
+    CATALOGUE_PREFIX = "catalogues/#{Rails.env}"
+    CONTAINER_CATALOG_PATH_PREFIX = "#{AZ_CONTAINER_NAME}/#{CATALOGUE_PREFIX}"
+    CONTAINER_IMAGE_PATH_PREFIX = "#{CATALOGUE_PREFIX}/images"
+
+    def self.container_name
+      AZ_CONTAINER_NAME
+    end
+
+    def self.training_image_path(image_url)
+      # this needs to not have the /training/ container prefix path
+      "#{CONTAINER_IMAGE_PATH_PREFIX}/#{File.basename(image_url)}"
+    end
+
+    def self.container_image_path(image_url)
+      prefix = ENV.fetch('TRAINING_PATH_PREFIX', "/#{Rails.env}")
+      "#{prefix}/#{File.basename(image_url)}"
+    end
+
+    def self.path_key(workflow_id)
+      "/#{CONTAINER_CATALOG_PATH_PREFIX}/workflow-#{workflow_id}"
+    end
   end
 end
--- spec/modules/zoobot_spec.rb
+++ spec/modules/zoobot_spec.rb
@@ -3,49 +3,53 @@
 require 'rails_helper'
 
 RSpec.describe Zoobot do
-  let(:image_url) do
-    'https://panoptes-uploads.zooniverse.org/subject_location/2f2490b4-65c1-4dca-ba25-c44128aa7a39.jpeg'
-  end
-
-  describe '.training_image_path' do
-    let(:expected_path) { 'training/catalogues/test/images/2f2490b4-65c1-4dca-ba25-c44128aa7a39.jpeg' }
-
-    it 'converts the url to a training container path' do
-      extracted_path = described_class.training_image_path(image_url)
-      expect(extracted_path).to eq(expected_path)
-    end
-  end
-
-  describe '.container_image_path' do
-    let(:expected_path) { '/test/2f2490b4-65c1-4dca-ba25-c44128aa7a39.jpeg' }
-
-    it 'converts the url to a training container path' do
-      extracted_path = described_class.container_image_path(image_url)
-      expect(extracted_path).to eq(expected_path)
-    end
-
-    it 'allows the env var to inject a prefix into the training container path' do
-      prefix = '/compute_dir/training_storage/training_images'
-      allow(ENV).to receive(:fetch).with('TRAINING_PATH_PREFIX', "/#{Rails.env}").and_return(prefix)
-      extracted_path = described_class.container_image_path(image_url)
-      expect(extracted_path).to eq("#{prefix}/2f2490b4-65c1-4dca-ba25-c44128aa7a39.jpeg")
-    end
-  end
-
-  describe '.storage_path_key' do
-    let(:exported_workflow_id) { 4 }
-    let(:expected_path_key) { "/training/catalogues/test/workflow-#{exported_workflow_id}" }
-
-    it 'converts the url to a training container path' do
-      extracted_path_key = described_class.storage_path_key(exported_workflow_id)
-      expect(extracted_path_key).to eq(expected_path_key)
-    end
-  end
-
   describe '.gz_label_column_headers' do
     it 'returns the correct list' do
       expected_column_headers = %w[id_str file_loc] | LabelExtractors::GalaxyZoo.question_answers_schema
       expect(described_class.gz_label_column_headers).to eq(expected_column_headers)
+    end
+  end
+end
+
+module Zoobot
+  RSpec.describe Storage do
+    let(:image_url) do
+      'https://panoptes-uploads.zooniverse.org/subject_location/2f2490b4-65c1-4dca-ba25-c44128aa7a39.jpeg'
+    end
+
+    describe '.training_image_path' do
+      let(:expected_path) { 'catalogues/test/images/2f2490b4-65c1-4dca-ba25-c44128aa7a39.jpeg' }
+
+      it 'converts the url to a training container path' do
+        extracted_path = described_class.training_image_path(image_url)
+        expect(extracted_path).to eq(expected_path)
+      end
+    end
+
+    describe '.container_image_path' do
+      let(:expected_path) { '/test/2f2490b4-65c1-4dca-ba25-c44128aa7a39.jpeg' }
+
+      it 'converts the url to a training container path' do
+        extracted_path = described_class.container_image_path(image_url)
+        expect(extracted_path).to eq(expected_path)
+      end
+
+      it 'allows the env var to inject a prefix into the training container path' do
+        prefix = '/compute_dir/training_storage/training_images'
+        allow(ENV).to receive(:fetch).with('TRAINING_PATH_PREFIX', "/#{Rails.env}").and_return(prefix)
+        extracted_path = described_class.container_image_path(image_url)
+        expect(extracted_path).to eq("#{prefix}/2f2490b4-65c1-4dca-ba25-c44128aa7a39.jpeg")
+      end
+    end
+
+    describe '.path_key' do
+      let(:exported_workflow_id) { 4 }
+      let(:expected_path_key) { "/training/catalogues/test/workflow-#{exported_workflow_id}" }
+
+      it 'converts the url to a training container path' do
+        extracted_path_key = described_class.path_key(exported_workflow_id)
+        expect(extracted_path_key).to eq(expected_path_key)
+      end
     end
   end
 end
