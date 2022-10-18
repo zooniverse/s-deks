5e47f2ed0a476e09cf304959a9d1c1ee15168f7d
Campbell Allen <campbell.allen@gmail.com>
2022-10-18 16:11:56 +0700

    Revert "add default to service job url"
    This reverts commit 13052a61f3e24024617a7bfaa4c04a05513cc776.

--- db/migrate/20221018051630_add_prediction_job_model.rb
+++ db/migrate/20221018051630_add_prediction_job_model.rb
@@ -1,7 +1,7 @@
 class AddPredictionJobModel < ActiveRecord::Migration[7.0]
   def change
     create_table :prediction_jobs do |t|
-      t.text   :service_job_url, default: ''
+      t.text   :service_job_url
       t.text   :manifest_url, null: false
       t.string :state, null: false
       t.text   :message, null: false, default: ''
