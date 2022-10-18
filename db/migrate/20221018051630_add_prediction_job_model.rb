e41d1cf0b1d0b19bee937c26505db0c9d6aa23df
Campbell Allen <campbell.allen@gmail.com>
2022-10-18 14:12:04 +0700

    remove not null constraint on service_job_url
    this will come after we create the prediction job and submit it to the batch system for processing

--- db/migrate/20221018051630_add_prediction_job_model.rb
+++ db/migrate/20221018051630_add_prediction_job_model.rb
@@ -1,4 +1,4 @@
-class AddBatchJobModel < ActiveRecord::Migration[7.0]
+class AddPredictionJobModel < ActiveRecord::Migration[7.0]
   def change
     create_table :prediction_jobs do |t|
       t.text :service_job_url
