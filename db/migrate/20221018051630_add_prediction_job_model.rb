ebffbae13ad3751821d3353d313f956d7a92d1f1
Campbell Allen <campbell.allen@gmail.com>
2022-10-18 14:10:47 +0700

    update migration filename

--- db/migrate/20221018051630_add_prediction_job_model.rb
+++ db/migrate/20221018051630_add_prediction_job_model.rb
@@ -1,7 +1,7 @@
 class AddBatchJobModel < ActiveRecord::Migration[7.0]
   def change
     create_table :prediction_jobs do |t|
-      t.text :service_job_url, null: false
+      t.text :service_job_url
       t.text :manifest_url, null: false
       t.timestamps
     end
