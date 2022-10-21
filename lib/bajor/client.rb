dc43449a15f900f74a933f56ea3784ea43028b55
Campbell Allen <campbell.allen@gmail.com>
2022-10-21 17:44:11 +0700

    add prediction_job_results method to bajor client
    used to fetch the results of the tasks and indicate success, failure or still running

--- lib/bajor/client.rb
+++ lib/bajor/client.rb
@@ -5,6 +5,7 @@ require 'httparty'
 module Bajor
   class Client
     class Error < StandardError; end
+    class PredictionJobTaskError < StandardError; end
 
     include HTTParty
     JSON_HEADERS = { 'Content-Type' => 'application/json', 'Accept' => 'application/json' }.freeze
@@ -12,6 +13,8 @@ module Bajor
     base_uri ENV.fetch('BAJOR_API_URL', 'https://bajor-staging.zooniverse.org')
 
     basic_auth ENV.fetch('BAJOR_BASIC_AUTH_USERNAME', 'bajor'), ENV.fetch('BAJOR_BASIC_AUTH_PASSWORD', 'bajor')
+
+    BAJOR_PREDICTION_BLOB_STORE_HOST_CONTAINER_URL = 'https://kadeactivelearning.blob.core.windows.net/predictions'
 
     def create_training_job(manifest_path)
       bajor_response = self.class.post(
@@ -39,6 +42,34 @@ module Bajor
 
       # return a bajor job tracking service URL
       bajor_prediction_job_tracking_url(bajor_response['id'])
+    end
+
+    def prediction_job_results(job_id)
+      bajor_response = self.class.get("/prediction/job/#{job_id}", headers: JSON_HEADERS)
+
+      # bajor returns 200 on successful submission
+      # anything else here will raise for notification at the callsite
+      raise_error(bajor_response) if bajor_response.code != 200
+
+      # loop over the task responses to collect the task results
+      all_tasks_results = bajor_response['tasks'].map do |task|
+        task.dig('execution_info', 'result')
+      end
+
+      if all_tasks_results.all?('success') # all tasks done and all good
+        job_results_env_setting = bajor_response['common_environment_settings'].find do |env_setting|
+          env_setting['name'] == 'PREDICTIONS_JOB_RESULTS_DIR'
+        end
+        # construct the prediction job results url
+        # TODO: this will most likely change format but will do for now
+        job_results_path_suffix = job_results_env_setting['value']
+        "#{BAJOR_PREDICTION_BLOB_STORE_HOST_CONTAINER_URL}/#{job_results_path_suffix}/predictions.csv"
+      elsif all_tasks_results.any?('failure') # one of the tasks failed :(
+        message = "One or more prediction job tasks failed - see job log details from: #{self.class.base_uri}/prediction/job/#{job_id}"
+        raise(PredictionJobTaskError, message)
+      elsif all_tasks_results.any?(&:nil?) # tasks are still running
+        nil
+      end
     end
 
     private
--- spec/lib/bajor/client_spec.rb
+++ spec/lib/bajor/client_spec.rb
@@ -144,5 +144,116 @@ RSpec.describe Bajor::Client do
         expect { bajor_client.create_prediction_job(manifest_url) }.to raise_error(Bajor::Client::Error, error_message)
       end
     end
+
+    describe 'prediction_job_results' do
+      let(:job_id) { '64bf4fab-ed6d-4f9a-b8ae-004086e3676f' }
+      let(:request_url) { "#{bajor_host}/prediction/job/#{job_id}" }
+      let(:task_execution_info) do
+        {
+          'result' =>	'success',
+          'start_time' =>	'2022-10-21T09:13:56.674045Z',
+          'end_time' =>	'2022-10-21T09:14:02.071913Z',
+          'exit_code' =>	0
+        }
+      end
+      let(:bajor_response_body) do
+        # these are partial responses from the bajor client
+        # for more details lookup a prediction job in bajor
+        # https://bajor-staging.zooniverse.org/prediction/jobs
+        # or
+        # https://learn.microsoft.com/en-us/rest/api/batchservice/job/get?tabs=HTTP#cloudjob
+        # https://learn.microsoft.com/en-us/rest/api/batchservice/job/get?tabs=HTTP#jobstate
+        {
+          'id' => '64bf4fab-ed6d-4f9a-b8ae-004086e3676f',
+          'uses_task_dependencies' => false,
+          'url' => 'https://zoobot.eastus.batch.azure.com/jobs/64bf4fab-ed6d-4f9a-b8ae-004086e3676f',
+          'e_tag' => '0x8DAB338D6CDA85D',
+          'last_modified' => '2022-10-21T07:49:52.968918Z',
+          'creation_time' => '2022-10-21T07:39:47.824823Z',
+          'state' => 'completed',
+          'state_transition_time' => '2022-10-21T07:49:53.759572Z',
+          'previous_state' => 'active',
+          'previous_state_transition_time' => '2022-10-21T07:39:47.854818Z',
+          'priority' => 0,
+          'allow_task_preemption' => false,
+          'max_parallel_tasks' => -1,
+          'common_environment_settings' => [
+            {
+              'name' => 'PREDICTIONS_JOB_RESULTS_DIR',
+              'value' => 'jobs/2022-10-21T07:39_64bf4fab-ed6d-4f9a-b8ae-004086e3676f/results'
+            }
+          ],
+          # Bajor helpfully includes the job tasks in it's job response
+          # https://learn.microsoft.com/en-us/rest/api/batchservice/task/get?tabs=HTTP
+          # https://learn.microsoft.com/en-us/rest/api/batchservice/task/get?tabs=HTTP#cloudtask
+          'tasks' => [
+            {
+              'id' => '1',
+              'state' => 'completed',
+              'state_transition_time' => '2022-10-21T07:49:50.213937Z',
+              'previous_state' => 'running',
+              'previous_state_transition_time' => '2022-10-21T07:49:32.360748Z',
+              'execution_info' => task_execution_info
+            }
+          ]
+        }
+      end
+      let(:results_url) do
+        'https://kadeactivelearning.blob.core.windows.net/predictions/jobs/2022-10-21T07:39_64bf4fab-ed6d-4f9a-b8ae-004086e3676f/results/predictions.csv'
+      end
+      let(:request) do
+        stub_request(:get, request_url).with(headers: request_headers)
+      end
+
+      context 'with a success response' do
+        before do
+          request.to_return(status: 200, body: bajor_response_body.to_json, headers: { content_type: 'application/json' })
+        end
+
+        it 'returns the job results blob storage url' do
+          result = bajor_client.prediction_job_results(job_id)
+          expect(result).to eq(results_url)
+        end
+      end
+
+      context 'when the task has not completed' do
+        let(:task_execution_info) do
+          {
+            'retryCount' =>	0,
+            'requeueCount' => 0
+          }
+        end
+
+        before do
+          request.to_return(status: 200, body: bajor_response_body.to_json, headers: { content_type: 'application/json' })
+        end
+
+        it 'returns nil' do
+          result = bajor_client.prediction_job_results(job_id)
+          expect(result).to be_nil
+        end
+      end
+
+      context 'with a failed response' do
+        # set the task to failued status indicator - from the failure job that's about to run
+        let(:task_execution_info) do
+          {
+            'result' =>	'failure',
+            'start_time' =>	'2022-10-21T09:13:56.674045Z',
+            'end_time' =>	'2022-10-21T09:14:02.071913Z',
+            'exit_code' =>	1
+          }
+        end
+
+        before do
+          request.to_return(status: 200, body: bajor_response_body.to_json, headers: { content_type: 'application/json' })
+        end
+
+        it 'raises an error' do
+          message = "One or more prediction job tasks failed - see job log details from: #{request_url}"
+          expect { bajor_client.prediction_job_results(job_id) }.to raise_error(Bajor::Client::PredictionJobTaskError, message)
+        end
+      end
+    end
   end
 end
