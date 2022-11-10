383c94afddf6d171a1dae1525f5acbe3454315cd
Campbell Allen <campbell.allen@gmail.com>
2022-11-10 08:48:51 +0700

    use prod bajor urls by default

--- lib/bajor/client.rb
+++ lib/bajor/client.rb
@@ -10,7 +10,7 @@ module Bajor
     include HTTParty
     JSON_HEADERS = { 'Content-Type' => 'application/json', 'Accept' => 'application/json' }.freeze
 
-    base_uri ENV.fetch('BAJOR_API_URL', 'https://bajor-staging.zooniverse.org')
+    base_uri ENV.fetch('BAJOR_API_URL', 'https://bajor.zooniverse.org')
 
     basic_auth ENV.fetch('BAJOR_BASIC_AUTH_USERNAME', 'bajor'), ENV.fetch('BAJOR_BASIC_AUTH_PASSWORD', 'bajor')
 
