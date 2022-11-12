b7155078186d80778b333c93587fdab90b1b5e3a
Campbell Allen <campbell.allen@gmail.com>
2022-11-12 14:29:38 +0700

    reset the message on submission
    in case this is a resubmission for error we want to clear the existing message

--- app/services/batch/prediction/create_job.rb
+++ app/services/batch/prediction/create_job.rb
@@ -15,7 +15,7 @@ module Batch
       def run
         begin
           bajor_job_url = bajor_client.create_prediction_job(prediction_job.manifest_url)
-          prediction_job.update(state: :submitted, service_job_url: bajor_job_url)
+          prediction_job.update(state: :submitted, service_job_url: bajor_job_url, message: '')
         rescue Bajor::Client::Error => e
           # mark the jobs as failed and record the client error message
           prediction_job.update(state: :failed, message: e.message)
