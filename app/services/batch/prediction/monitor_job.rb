23c4bc7e06592c4751d5366b5900c97bc425c67c
Campbell Allen <campbell.allen@gmail.com>
2022-12-12 20:32:33 +1000

    remove the out of date comment

--- app/services/batch/prediction/monitor_job.rb
+++ app/services/batch/prediction/monitor_job.rb
@@ -15,9 +15,6 @@ module Batch
       def run
         begin
           if (results_url = bajor_client.prediction_job_results(prediction_job.job_id))
-            # TODO: longer term we will have to combine / format the results file
-            # to a useful format with a json schem describing the results data
-            # however let's return it for now as a csv file
             prediction_job.update!(state: :completed, results_url: results_url)
           end
         rescue Bajor::Client::PredictionJobTaskError => e
