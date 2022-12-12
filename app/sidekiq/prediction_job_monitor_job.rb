493a487a09d6b228b0e4442a6a253a73df1e495a
Campbell Allen <campbell.allen@gmail.com>
2022-12-12 22:29:02 +1000

    kick off a job to process prediction results when a run is completed

--- app/sidekiq/prediction_job_monitor_job.rb
+++ app/sidekiq/prediction_job_monitor_job.rb
@@ -11,12 +11,12 @@ class PredictionJobMonitorJob
     # short circuit if we run this on a job that's already done
     return if prediction_job.completed?
 
-    # TBC what this interface returns, perahps a result object?
-    # or it composes another service to do the work of aggregating the prediction job results
-    # and it can't mark this job as successful until that is done?
     prediction_job = Batch::Prediction::MonitorJob.new(prediction_job).run
 
-    return if prediction_job.completed?
+    if prediction_job.completed?
+      ProcessPredictionResultsJob.perform_async(prediction_job.id)
+      return # don't reschedule this job if it's done
+    end
 
     # reschedule this job to run again in 1 minute
     PredictionJobMonitorJob.perform_in(MONITOR_JOB_RESCHEDULE_DELAY.minute, prediction_job.id)
--- spec/sidekiq/prediction_job_monitor_job_spec.rb
+++ spec/sidekiq/prediction_job_monitor_job_spec.rb
@@ -11,12 +11,13 @@ RSpec.describe PredictionJobMonitorJob, type: :job do
         state: :submitted
       )
     end
+    let(:prediction_job_monitor_results) { PredictionJob.new(id: -1) }
 
     let(:prediction_job_monitor_service) { instance_double(Batch::Prediction::MonitorJob) }
     let(:job) { described_class.new }
 
     before do
-      allow(prediction_job_monitor_service).to receive(:run).and_return(prediction_job)
+      allow(prediction_job_monitor_service).to receive(:run).and_return(prediction_job_monitor_results)
       allow(Batch::Prediction::MonitorJob).to receive(:new).and_return(prediction_job_monitor_service)
       allow(prediction_job).to receive(:completed?).and_return(false)
     end
@@ -27,19 +28,30 @@ RSpec.describe PredictionJobMonitorJob, type: :job do
     end
 
     it 'reschedules itself if the job has not fininshed' do
-      allow(prediction_job).to receive(:completed?).and_return(false)
-      allow(PredictionJobMonitorJob).to receive(:perform_in)
+      allow(prediction_job_monitor_results).to receive(:completed?).and_return(false)
+      allow(described_class).to receive(:perform_in)
       job.perform(prediction_job.id)
-      expect(PredictionJobMonitorJob).to have_received(:perform_in).with(1.minute, prediction_job.id)
+      expect(described_class).to have_received(:perform_in).with(1.minute, prediction_job_monitor_results.id)
     end
 
-    context 'when the prediction job has been completed' do
+    context 'when the monitor job returns a completed job' do
       before do
-        allow(prediction_job).to receive(:completed?).and_return(false)
+        allow(prediction_job_monitor_results).to receive(:completed?).and_return(true)
+      end
+
+      it 'schedules a process prediction results job' do
+        allow(ProcessPredictionResultsJob).to receive(:perform_async)
+        job.perform(prediction_job.id)
+        expect(ProcessPredictionResultsJob).to have_received(:perform_async).with(prediction_job_monitor_results.id)
+      end
+    end
+
+    context 'when the prediction job has been already completed' do
+      before do
+        prediction_job.update_column(:state, 'completed')
       end
 
       it 'does not run the prediction job monitor service' do
-        prediction_job.update_column(:state, :completed)
         job.perform(prediction_job.id)
         expect(prediction_job_monitor_service).not_to have_received(:run)
       end
