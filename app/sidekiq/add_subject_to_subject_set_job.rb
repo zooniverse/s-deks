bc0ad1da076beb341aeaf5ac43d9d12a125b9fbd
Campbell Allen <campbell.allen@gmail.com>
2022-12-13 19:12:32 +1000

    add oauth client credentials authentication to panoptes client
    ensure the panoptes client acts on the behalf of a user with rights on the projects

--- app/sidekiq/add_subject_to_subject_set_job.rb
+++ app/sidekiq/add_subject_to_subject_set_job.rb
@@ -10,7 +10,16 @@ class AddSubjectToSubjectSetJob
   private
 
   def panoptes_client
-    # TODO: this will need auth setup
-    @panoptes_client ||= Panoptes::Client.new(env: Rails.env.to_s)
+    @panoptes_client ||= Panoptes::Client.new(
+      env: Rails.env.to_s,
+      # setup via oauth applications page - https://panoptes.zooniverse.org/oauth/applications
+      auth: {
+        client_id: ENV.fetch('PANOPTES_OAUTH_CLIENT_ID'),
+        client_secret: ENV.fetch('PANOPTES_OAUTH_CLIENT_SECRET')
+      },
+      # without this the owner of the oauth application needs to be have
+      # collaborator rights on each project we want to modify
+      params: { admin: true }
+    )
   end
 end
