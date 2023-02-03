8e3fd1e159cf9e0dfa5fc17376774604b9864650
Campbell Allen <campbell.allen@gmail.com>
2023-02-03 13:36:02 +1000

    use the new panoptes api client factory

--- app/sidekiq/add_subject_to_subject_set_job.rb
+++ app/sidekiq/add_subject_to_subject_set_job.rb
@@ -4,22 +4,6 @@ class AddSubjectToSubjectSetJob
   include Sidekiq::Job
 
   def perform(subject_id, subject_set_id)
-    panoptes_client.add_subjects_to_subject_set(subject_set_id, subject_id)
-  end
-
-  private
-
-  def panoptes_client
-    @panoptes_client ||= Panoptes::Client.new(
-      env: Rails.env.to_s,
-      # setup via oauth applications page - https://panoptes.zooniverse.org/oauth/applications
-      auth: {
-        client_id: ENV.fetch('PANOPTES_OAUTH_CLIENT_ID'),
-        client_secret: ENV.fetch('PANOPTES_OAUTH_CLIENT_SECRET')
-      },
-      # without this the owner of the oauth application needs to be have
-      # collaborator rights on each project we want to modify
-      params: { admin: true }
-    )
+    Panoptes::Api.client.add_subjects_to_subject_set(subject_set_id, subject_id)
   end
 end
