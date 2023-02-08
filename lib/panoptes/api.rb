8307aa09ef8f11bff4149472c2f86f6a3365519f
Campbell Allen <campbell.allen@gmail.com>
2023-02-08 16:57:09 +1000

    use async faraday as default client

--- lib/panoptes/api.rb
+++ lib/panoptes/api.rb
@@ -3,12 +3,17 @@
 module Panoptes
   class Api
     def self.client
+      require 'async/http/faraday'
+
+      # override the default adapter to use async requests
+      Faraday.default_adapter = :async_http
+
       Panoptes::Client.new(
         env: Rails.env.to_s,
         # setup via oauth applications page - https://panoptes.zooniverse.org/oauth/applications
         auth: {
-          client_id: ENV.fetch('PANOPTES_OAUTH_CLIENT_ID'),
-          client_secret: ENV.fetch('PANOPTES_OAUTH_CLIENT_SECRET')
+          client_id: ENV.fetch('PANOPTES_OAUTH_CLIENT_ID', nil),
+          client_secret: ENV.fetch('PANOPTES_OAUTH_CLIENT_SECRET', nil)
         },
         # without this the owner of the oauth application needs to be have
         # collaborator rights on each project we want to modify
