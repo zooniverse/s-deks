94b817bd29935413bb65acda4050cdca6ca878ca
Campbell Allen <campbell.allen@gmail.com>
2023-02-09 11:57:53 +1000

    ignore auth in dev env setup

--- lib/panoptes/api.rb
+++ lib/panoptes/api.rb
@@ -8,13 +8,18 @@ module Panoptes
       # override the default adapter to use async requests
       Faraday.default_adapter = :async_http
 
+      # ignore auth for dev env
+      auth = {} if Rails.env.development?
+      # setup client creds for all other envs
+      auth ||= {
+        client_id: ENV.fetch('PANOPTES_OAUTH_CLIENT_ID'),
+        client_secret: ENV.fetch('PANOPTES_OAUTH_CLIENT_SECRET')
+      }
+
       Panoptes::Client.new(
         env: Rails.env.to_s,
         # setup via oauth applications page - https://panoptes.zooniverse.org/oauth/applications
-        auth: {
-          client_id: ENV.fetch('PANOPTES_OAUTH_CLIENT_ID', nil),
-          client_secret: ENV.fetch('PANOPTES_OAUTH_CLIENT_SECRET', nil)
-        },
+        auth: auth,
         # without this the owner of the oauth application needs to be have
         # collaborator rights on each project we want to modify
         params: { admin: true }
