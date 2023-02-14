d08febd8d65c15b39d16a20fc6e5f02b2f028cdf
Campbell Allen <campbell.allen@gmail.com>
2023-02-14 19:46:27 +1000

    add PredictionManifestExportJob once the training job is complete
    ensure we use the newly trained model in a prediciton loop for the active set of subjects.

--- app/sidekiq/training_job_monitor_job.rb
+++ app/sidekiq/training_job_monitor_job.rb
@@ -14,9 +14,19 @@ class TrainingJobMonitorJob
     training_job = Batch::Training::MonitorJob.new(training_job).run
 
     # avoid rescheduling the job if it's failed or completed
-    return if training_job.failed? || training_job.completed?
+    return if training_job.failed?
 
-    # reschedule this job to run again in 1 minute
-    TrainingJobMonitorJob.perform_in(MONITOR_JOB_RESCHEDULE_DELAY.minute, training_job.id)
+    if training_job.completed?
+      # As the training job is finished- we want to run the prediction system using the newly trained model
+      # so we call the prediction manifest export job to create a manifest and submit it for batch processing
+      #
+      # NOTE: this jobs allows us to override the subject set id
+      # if we want to create a prediction manifest for a specific subject set
+      # and not just the Default GZ Active Learning Loop system setup.
+      PredictionManifestExportJob.perform_async
+    else
+      # reschedule this job to run again in 1 minute
+      TrainingJobMonitorJob.perform_in(MONITOR_JOB_RESCHEDULE_DELAY.minute, training_job.id)
+    end
   end
 end
