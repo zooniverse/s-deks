5a23b65c97203d7f5ea17a47dc066003b06b3b8a
Campbell Allen <campbell.allen@gmail.com>
2023-02-23 16:08:57 +1000

    switch to explicit context job params

--- app/sidekiq/retrain_zoobot_job.rb
+++ app/sidekiq/retrain_zoobot_job.rb
@@ -8,21 +8,18 @@ class RetrainZoobotJob
   RECENT_TRAINING_EXPORT_WINDOW = ENV.fetch('RECENT_TRAINING_EXPORT_WINDOW', 12).to_i
   TRAINING_JOB_MONITOR = ENV.fetch('TRAINING_JOB_MONITOR', 10).to_i
 
-  def perform(workflow_id = nil)
-    # allow the workflow_id to be optional and default a known value
-    # GZ staging test project - https://caesar-staging.zooniverse.org/workflows/3598
-    # GZ production project   - https://caesar.zooniverse.org/workflows/21802
-    workflow_id ||= Context.find(ENV.fetch('ZOOBOT_GZ_CONTEXT_ID')).workflow_id
+  def perform(context_id)
+    training_context = Context.find(context_id)
 
     # see if we have a recent re-usable data export instead of making one each time
     # the data should be similar and the window period is configurable
-    training_data_export = find_recent_training_data_export(workflow_id)
+    training_data_export = find_recent_training_data_export(training_context.workflow_id)
 
     # if we haven't found a recent training data export then create one
     unless training_data_export
       training_data_export = TrainingDataExport.create!(
-        storage_path: TrainingDataExport.storage_path(workflow_id),
-        workflow_id: workflow_id
+        storage_path: TrainingDataExport.storage_path(training_context.workflow_id),
+        workflow_id: training_context.workflow_id
       )
 
       # run the export service code to create the training data export catalogue on blob storage system
@@ -30,7 +27,7 @@ class RetrainZoobotJob
     end
 
     # create a new training job record to track the batch training job
-    training_job = create_training_job(training_data_export.storage_path, workflow_id)
+    training_job = create_training_job(training_data_export.storage_path, training_context.workflow_id)
     # submit the export training job to the batch training service
     # this updates the training job state
     training_job = Batch::Training::CreateJob.new(training_job).run
@@ -43,7 +40,7 @@ class RetrainZoobotJob
     # kick off a job monitor here that updates the training job resource with the job tasks results
     # this background job will reschedule itself until the training job is completed
     # and handle the completion / failure events for the training job
-    TrainingJobMonitorJob.perform_in(TRAINING_JOB_MONITOR.minutes, training_job.id)
+    TrainingJobMonitorJob.perform_in(TRAINING_JOB_MONITOR.minutes, training_job.id, training_context.id)
 
     training_job
   end
--- spec/sidekiq/retrain_zoobot_job_spec.rb
+++ spec/sidekiq/retrain_zoobot_job_spec.rb
@@ -4,8 +4,10 @@ require 'rails_helper'
 
 RSpec.describe RetrainZoobotJob, type: :job do
   describe 'perform' do
+    fixtures :contexts
+
     let(:job) { described_class.new }
-    let(:workflow_id) { -1 }
+    let(:context) { Context.first }
     let(:export_training_data_double) { instance_double(Export::TrainingData) }
     let(:batch_training_create_job_double) { instance_double(Batch::Training::CreateJob) }
     let(:training_job) { TrainingJob.new(id: -1) }
@@ -19,28 +21,28 @@ RSpec.describe RetrainZoobotJob, type: :job do
     end
 
     it 'creates the training data export resource' do
-      expect { job.perform(workflow_id) }.to change(TrainingDataExport, :count).by(1)
+      expect { job.perform(context.id) }.to change(TrainingDataExport, :count).by(1)
     end
 
     it 'runs the training data export service' do
-      job.perform(workflow_id)
+      job.perform(context.id)
       expect(export_training_data_double).to have_received(:run).once
     end
 
     it 'creates the training job resource for monitoring' do
-      job.perform(workflow_id)
+      job.perform(context.id)
       expect(TrainingJob).to have_received(:create!).once
     end
 
     it 'runs the batch training create job service' do
-      job.perform(workflow_id)
+      job.perform(context.id)
       expect(batch_training_create_job_double).to have_received(:run).once
     end
 
     it 'queues a TrainingJobMonitorJob in the background' do
       allow(TrainingJobMonitorJob).to receive(:perform_in)
-      job.perform(workflow_id)
-      expect(TrainingJobMonitorJob).to have_received(:perform_in).with(10.minutes, training_job.id)
+      job.perform(context.id)
+      expect(TrainingJobMonitorJob).to have_received(:perform_in).with(10.minutes, training_job.id, context.id)
     end
 
     context 'when the training job failed to submit' do
@@ -50,37 +52,20 @@ RSpec.describe RetrainZoobotJob, type: :job do
 
       it 'does not queue a TrainingJobMonitorJob in the background if job fails to submit', :aggregate_failures do
         allow(TrainingJobMonitorJob).to receive(:perform_in)
-        expect { job.perform(training_job.id) }.to raise_error(RetrainZoobotJob::Failure)
+        expect { job.perform(context.id) }.to raise_error(RetrainZoobotJob::Failure)
         expect(TrainingJobMonitorJob).not_to have_received(:perform_in)
-      end
-    end
-
-    describe 'allow the job to load the default context workflow id' do
-      fixtures :contexts
-      let(:context) { Context.first }
-      let(:workflow_id) { context.workflow_id }
-      let(:storage_path) { TrainingDataExport.storage_path(workflow_id) }
-
-      before do
-        allow(ENV).to receive(:fetch).with('ZOOBOT_GZ_CONTEXT_ID').and_return(context.id)
-      end
-
-      it 'defaults the workflow_id to a known env var' do
-        allow(TrainingDataExport).to receive(:create!).and_return(TrainingDataExport.new)
-        job.perform
-        expect(TrainingDataExport).to have_received(:create!).with(storage_path: storage_path, workflow_id: workflow_id)
       end
     end
 
     context 'with existing training data exports' do
       it 'finds and reuses the existing training data export created less than 12 hours ago' do
-        training_data_export = TrainingDataExport.create!(workflow_id: workflow_id, created_at: 11.hours.ago, storage_path: 'test', state: :finished)
-        expect(job.find_recent_training_data_export(workflow_id)).to eq(training_data_export)
+        training_data_export = TrainingDataExport.create!(workflow_id: context.workflow_id, created_at: 11.hours.ago, storage_path: 'test', state: :finished)
+        expect(job.find_recent_training_data_export(context.workflow_id)).to eq(training_data_export)
       end
 
-      it 'does not find or reus and existing training data export created more than 12 hours ago' do
-        TrainingDataExport.create!(workflow_id: workflow_id, created_at: (12.hours + 1.minute).ago, storage_path: 'test', state: :finished)
-        expect(job.find_recent_training_data_export(workflow_id)).to be_nil
+      it 'does not find or reuse an existing training data export created more than 12 hours ago' do
+        TrainingDataExport.create!(workflow_id: context.workflow_id, created_at: (12.hours + 1.minute).ago, storage_path: 'test', state: :finished)
+        expect(job.find_recent_training_data_export(context.workflow_id)).to be_nil
       end
     end
   end
