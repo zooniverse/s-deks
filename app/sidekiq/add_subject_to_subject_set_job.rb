46b86997c317bd8ef34fd8adb865495eeee926ca
Campbell Allen <campbell.allen@gmail.com>
2023-02-24 16:43:07 +1000

    use Sync to wrap API async calls and ensure we close the connections
    https://socketry.github.io/async/guides/getting-started/index.html#synchronous-execution-in-an-existing-fiber-scheduler

--- app/sidekiq/add_subject_to_subject_set_job.rb
+++ app/sidekiq/add_subject_to_subject_set_job.rb
@@ -3,17 +3,21 @@
 class AddSubjectToSubjectSetJob
   include Sidekiq::Job
 
-  def perform(subject_ids, subject_set_id, max_retries=3)
+  def perform(subject_ids, subject_set_id, max_retries = 3)
     retries ||= 1
-    Panoptes::Api.client.add_subjects_to_subject_set(subject_set_id, Array.wrap(subject_ids))
-  rescue Panoptes::Client::ServerError => e
-    # handle intermittent API errors like the following
-    # e.g. {"errors"=>[{"message"=>"Attempted to update a stale object: SubjectSet."}]}
-    retries += 1
-    raise e if retries > max_retries
+    Sync do
+      Panoptes::Api.client.add_subjects_to_subject_set(subject_set_id, Array.wrap(subject_ids))
+    rescue Panoptes::Client::ServerError => e
+      # handle intermittent API errors like the following
+      # e.g. {"errors"=>[{"message"=>"Attempted to update a stale object: SubjectSet."}]}
+      retries += 1
+      raise e if retries > max_retries
 
-    # sleep for a random amount of time between 0 and max_retries and then retry the API client operation
-    sleep(rand(max_retries))
-    retry
+      # sleep for a random amount of time between 0 and max_retries and then retry the API client operation
+      sleep(rand(max_retries))
+      retry
+    ensure
+      Faraday.default_connection.close
+    end
   end
 end
--- app/sidekiq/remove_subject_from_subject_set_job.rb
+++ app/sidekiq/remove_subject_from_subject_set_job.rb
@@ -14,15 +14,19 @@ class RemoveSubjectFromSubjectSetJob
     # use the underlying client implementation to achieve it
     # longer term I should add this to the client...
     # apologies to all future selves for not doing so this time :sadpanda:
-    Panoptes::Api.client.panoptes.delete("/subject_sets/#{subject_set_id}/links/subjects/#{linked_subject_ids}")
-  rescue Panoptes::Client::ServerError => e
-    # handle intermittent API errors like the following
-    # e.g. {"errors"=>[{"message"=>"Attempted to update a stale object: SubjectSet."}]}
-    retries += 1
-    raise e if retries > max_retries
+    Sync do
+      Panoptes::Api.client.panoptes.delete("/subject_sets/#{subject_set_id}/links/subjects/#{linked_subject_ids}")
+    rescue Panoptes::Client::ServerError => e
+      # handle intermittent API errors like the following
+      # e.g. {"errors"=>[{"message"=>"Attempted to update a stale object: SubjectSet."}]}
+      retries += 1
+      raise e if retries > max_retries
 
-    # sleep for a random amount of time between 0 and max_retries and then retry the API client operation
-    sleep(rand(max_retries))
-    retry
+      # sleep for a random amount of time between 0 and max_retries and then retry the API client operation
+      sleep(rand(max_retries))
+      retry
+    ensure
+      Faraday.default_connection.close
+    end
   end
 end
